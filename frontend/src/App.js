03b2b73312ef197f4c2de8afe59cff9e281af742
Le Kibbitz <thomas.joannes@gmail.com>
2025-07-01 21:00:24 +0200

    Update App.js
    Section Arbitrage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>BridgeFacile - Documentation d'Arbitrage</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenue dans la section d'arbitrage de BridgeFacile.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <p>Cette section contiendra le Code International, le RNC et les catégories d'enchères.</p>
+      </main>
     </div>
   );
 }
