29f954b3c98736d4de928df25fc98bb807bdbd6d
Le Kibbitz <thomas.joannes@gmail.com>
2025-07-02 01:08:40 +0200

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ⚠️ REMPLACEZ par vos vraies valeurs Supabase
-const supabaseUrl = 'https://supabase.com/dashboard/project/aqokcjsmajnpfkladubp'
+const supabaseUrl = 'https://aqokcjsmajnpfkladubp.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxb2tjanNtYWpucGZrbGFkdWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzOTg4OTUsImV4cCI6MjA2Njk3NDg5NX0.f2Z4aRkTlUExrpir4fqIpn0TkzpS5B0WPuAHAb26YFM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
